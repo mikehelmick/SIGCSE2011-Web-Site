17e7fa9f265379a06b06c34a85b6fe11a99c7851
Mike Helmick <mike.helmick@gmail.com>
2010-08-05 21:25:28 -0700

    Update meta-reviws page.

--- reviewers/metareviews.php
+++ reviewers/metareviews.php
@@ -6,221 +6,91 @@
 
 <h1>The Meta-Review Process for Paper Submissions</h1>
 
-            <p>
-            SIGCSE has been progressively improving our reviewing process
-            in response to community requests, e.g., [1,2,3].
-            At the same time, the number of submissions to SIGCSE has risen
-            over time.  (Last year, there were 2 Program Chairs,
-            324 paper submissions, and over 1500 reviews.)
-            </p>
-
-            <p>
-            To continue the process of improvement, to provide authors
-            with more information on how the Program Chairs use reviews
-            to analyze the paper and how authors can use reviews
-            constructively in case of resubmission, and to mitigate the
-            increasing burden on the Program Committee chairs,
-            we have established a prototype paper reviewing process
-            for SIGCSE 2009.  This page explains how the new process
-            affects each of the constituencies.
-            </p>
-
+<p>SIGCSE strives for the best reviewing process that we can achieve. In
+order to assist the Program Chairs in dealing with the high volume of
+submissions that we receive, SIGCSE uses a meta-review process to help
+summarize the various reviews. The following description indicates how
+meta-reviews play into the various stages of the reviewing process.</p>
 
 <div class="SectionHeader"><div class="Full">
-<h2>Authors</h2>
-</div></div>   
-            <p>
-            <!--
-            Authors submit papers for double-blind review.
-            The goal of the anonymized version is to, as much as possible,
-            allow the author(s) of the paper an unbiased review.
-            The authors choose the level of anonymity in the paper.
-            At the lowest level, they may choose to use no anonymization;
-            at the highest level, they may remove <strong>all</strong>
-            references to the authors (including author's names and
-            affiliation plus identifying information within the body
-            of the paper such as websites or related publications). -->
-            After reviewing is complete, authors will see their reviews
-            as in previous years.
-            </p>
- 
-            <p>Authors will also receive meta-reviews that synthesize the
-            existing reviews, clarifying how the program committee
-            interprets the disparate information from individual reviews.
-            </p>
-
-
-<div class="SectionHeader"><div class="Full">
-<h2>Reviewers</h2>
-</div></div>
-            <p>
-            At least four reviewers review each paper.
-            Reviewers are drawn from the SIGCSE reviewer pool
-            with preference for those whose expertise match the
-            paper's topic areas.
-            </p>
-
-            <p>
-            A review must objectively, accurately, and clearly assess
-            a paper's suitability for publication at SIGCSE,
-            founded in the reviewer's disciplinary expertise
-            and on the basis of the written paper's originality,
-            technical soundness, contribution to computer science
-            education, and clarity of presentation.
-            </p>
-
-            <p>Reviews that do not meet this standard may be deleted.
-            For example, an unacceptable review might:
-            </p>
-
-            <ul>
-              <li> be incoherent, unreadable, or irrelevant to the paper;
-                   </li>
-
-              <li> focus on the paper's topic area or presumed authors
-                   at the expense of assessment of the paper itself; or
-                   </li>
-
-              <li> provide no justification for its numeric ratings.
-                   (Even in &quot;obvious&quot; cases, reviewers should
-                   briefly justify ratings.)
-                   </li>
-            </ul>
-
-            <p>
-            Reviewers whose reviews are consistently unacceptable
-            may be permanently disbarred from SIGCSE reviewing.
-            </p>
-
-            <p>
-            Note: A difference in rating or opinion with other reviewers
-            or with Program Committee members will <strong>never</strong>
-            be cause for deletion of a review.
-            </p>
-
-
-<div class="SectionHeader"><div class="Full">
-<h2>Associate Program Chairs</h2>
-</div></div>
-            <p>One of the six Associate Program Chairs will synthesize
-            each paper's reviews into a &quot;meta-review&quot;
-            to improve accuracy, quality, and effectiveness
-            of the selection process.
-            </p>
-
-            <p>
-            Associate Program Chairs were selected by the Program Chairs
-            for their disciplinary expertise.  Associate Program Chairs
-            read the papers' reviews and abstracts though not,
-            in most cases, the paper itself.
-            </p>
-
-            <p>
-            Associate Program Chairs will:
-            </p>
-
-            <ol>
-              <li> Delete, in the rare cases described above,
-                   unacceptable reviews.  
-                   </li>
-
-              <li> Usually, summarize the case for and against accepting
-                   a paper based on its abstract, reviews, and possibly
-                   its contents.
-
-                   <p style="font-size: 1.0em">
-                   Sometimes, summarize only the case for accepting
-                   if reviewers' ratings are consistently high,
-                   or analogously, only the case against accepting when
-                   ratings are consistently low.
-                   </p>
-
-                   <p style="font-size: 1.0em">
-                   In rare cases (e.g., when the reviews are deemed
-                   irreconcilably disparate or when too few reviewers
-                   claim familiarity with the area), read the paper fully
-                   and provide a &quot;normal&quot; review, in addition
-                   to the meta-review.
-                   </p>
-                   </li>
-
-              <li> Rate the paper on the basis of existing reviews.
-                   Associate Program Chairs are charged with making strong
-                   recommendations; thus, their ratings will often be 1s
-                   or 6s.  They will briefly justify the rating in the
-                   meta-review.
-                   </li>
-            </ol>
-
-
-            <br />
-            <hr />
-
-<div class="SectionHeader"><div class="Full">
-<h2>Program Chairs</h2>
+<h2>Stage 1: Reviewers</h2>
 </div></div>
 
-            <p>
-            The Program Chairs' duty, quoted from
-            <a href="http://sigcse.org/conferences/ProgramChair.shtml">SIGCSE's Program Chair Responsibility Policy</a>,
-            is &quot;to create a good, well balanced program at the SIGCSE
-            Technical Symposium and the ITiCSE conference.  The program
-            should provide both short and long term benefits
-            to the conference attendees and the wider community.&quot;
-            </p>
+<p>At least four reviewers review each paper. Reviewers are drawn from the
+SIGCSE reviewer pool with preference for those whose expertise match the paper's topic areas.</p>
 
-            <p>
-            Within the framework described here, that means ensuring
-            overall quality in the reviewing process and assembling
-            an excellent program that reflects reviewers' and
-            Associate Program Chairs' decisions with respect to
-            the overall composition, exigencies, and special focus
-            of the conference.
-            </p>
+<p>A review must objectively, accurately, and clearly assess a paper's
+suitability for publication at SIGCSE, founded in the reviewer's
+disciplinary expertise and on the basis of the written paper's
+originality, technical soundness, contribution to computer science
+education, and clarity of presentation.</p>
 
-            <p>
-            Program Chairs make all final decisions on accepting or
-            rejecting papers on the basis of the papers, their reviews,
-            their meta-reviews,
-            and a global effort to compose an excellent program.
-            </p>
+<p>Reviews that do not meet this standard may be deleted. For example, an
+unacceptable review might:
+<ul>
+ <li>be incoherent, unreadable, or irrelevant to the paper;</li>
+ <li>focus on the paper's topic area or presumed authors at the expense of assessment of the paper itself; or</li>
+ <li>provide no justification for its numeric ratings. (Even in "obvious" cases, reviewers should briefly justify ratings.)</li>
+</ul>
+</p> 
 
-            <p>
-            A Program Chair will read every meta-review of every
-            submitted paper.  In many cases, Program Chairs will also
-            consult abstracts, reviews, or the papers themselves.
-            </p>
+<p>Reviewers whose reviews are consistently unacceptable may be permanently disbarred from SIGCSE reviewing.</p>
 
-            <p>
-            Finally, the Program Chairs will report to the community
-            --- in the proceedings or at the business meeting --- 
-            on the aggregate outcome of the process and its
-            potential for revision or extension in coming years.
-            </p>
+<p>Note: A difference in rating or opinion with other reviewers or with
+Program Committee members will never be cause for deletion of a review.</p>
 
 <div class="SectionHeader"><div class="Full">
-<h3>References</h3>
+<h2>Stage 2: Associate Program Chairs (The Meta-Reviewers)</h2>
 </div></div>
 
+<p>After reviews have been submitted, one of the Associate Program Chairs
+synthesizes each paper's reviews into a "meta-review" to improve
+accuracy, quality, and effectiveness of the selection process.</p>
 
-            <ol>
-              <li> Program Chair Responsibility Policy,
-                   <a href="http://sigcse.org/conferences/ProgramChair.shtml">http://sigcse.org/conferences/ProgramChair.shtml</a>.
-                   </li>
+<p>Associate Program Chairs are selected by the Program Chairs for their
+disciplinary expertise. Associate Program Chairs read the papers'
+reviews and abstracts though not, in most cases, the paper itself.</p>
 
-              <li> Dan Joyce, Bruce Klein, Rene McCauley.
-                   Report on improving SIGCSE/ITiCSE program creation process,
-                   SIGCSE Bulletin, 39(4) (December 2007), p. 128-129.
-                   </li>
+<p>After an APC has read the reviews for a particular paper, that APC has the following responsibilities:
+<ol>
+  <li>In the rare case that a review clearly violated SIGCSE's review standards (as described above), the APC may recommend to the Program Chairs that the review be deleted.</li>
+  <li>The APC summarizes the case for and against a paper depending on its reviews, abstract, and occasionally the paper itself. In cases where the reviews are clearly in agreement, only the case made by the reviewers (for or against) is summarized. In rare cases (e.g., when the reviews are deemed irreconcilably disparate or when too few reviewers claim familiarity with the area), the APC may read the paper fully and provide a "normal" review, in addition to the meta-review.</li>
+  <li>APCs are charged with making strong recommendations and therefore are encouraged to use the high (5,6) and low (1,2) ratings in most cases (as opposed to regular reviewers). APC reviews are NOT reviews of the paper, nor are their ratings intended to be averages; instead, they are reviews of the reviews to help the Program Chairs make decisions on the final program.</li>
+</ol>
+</p>
 
-              <li> Amruth Kumar, Patricia A. Joseph, Paul Wagner,
-                   and Michael Goldweber.
-                   It is Time to Review the SIGCSE Reviewing Process.
-                   In Proceedings of the Thirty-Eighth SIGCSE Technical
-                   Symposium on Computer Science Education, page 591,
-                   Covington, Kentucky, USA, March 2007.
-                   </li>
-            </ol>
+<div class="SectionHeader"><div class="Full">
+<h2>Stage 3: Program Chairs</h2>
+</div></div>
+
+<p>The Program Chairs' duty, quoted from <a href="http://sigcse.org/conferences/ProgramChair.shtml">SIGCSE's Program Chair
+Responsibility Policy</a>, is "to create a good, well balanced
+program at the SIGCSE Technical Symposium and the ITiCSE conference. The
+program should provide both short and long term benefits to the
+conference attendees and the wider community."</p>
+
+<p>Within the framework described here, that means ensuring overall quality
+in the reviewing process and assembling an excellent program that
+reflects reviewers' and Associate Program Chairs' decisions with respect
+to the overall composition, exigencies, and special focus of the conference.</p>
+
+<p>Program Chairs make all final decisions on accepting or rejecting papers on the basis of the papers, their reviews, their meta-reviews, and a global effort to compose an excellent program that is balanced and diverse across a variety of topic areas.</p>
+
+<p>Program Chairs read every meta-review of every submitted paper. In many
+cases, Program Chairs will also consult abstracts, reviews, or the
+papers themselves.</p>
+
+<p>Finally, the Program Chairs will report to the community --- in the
+proceedings or at the business meeting --- on the aggregate outcome of
+the reviewing process and its potential for revision or extension in
+coming years.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>Stage 4: Authors</h2>
+</div></div>
+
+<p>After reviewing is complete, authors see the reviews for their proposal.
+Authors also receive meta-reviews that synthesize the existing reviews, clarifying how the program committee interprets the disparate information from individual reviews.</p>
 
 
 <div class="SectionHeader"><div class="Full">
