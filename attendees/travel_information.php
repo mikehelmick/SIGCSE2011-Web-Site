abe1153dd7f17125a3567af7ef2da211fd760534
Mike Helmick <mike.helmick@gmail.com>
2010-10-26 21:39:16 -0700

    added travel informaiton page

---
+++ attendees/travel_information.php
@@ -0,0 +1,89 @@
+<?php
+ $title = "SIGCSE 2011 - Travel Information";
+ $menu = "attendees";
+ include("../parts/top.php");
+?>       
+     
+<h1>SIGCSE 2011 Travel Information</h1> 
+ 
+<h3>Contents:</h3> 
+  <ul> 
+    <li><a href="#airline">Airline Service</a></li> 
+    <li><a href="#shuttle">Ground Transportation</a></li> 
+    <li><a href="#rentals">Rail Service</a></li> 
+    <li><a href="#parking">Driving and Parking at the Sheraton Dallas</a></li> 
+  </ul> 
+
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="airline">Airline Service</a></h2>
+</div></div>
+<br/>
+
+<p>Commercial airline service to Dallas is handled by two major airports:
+<ul>
+<li>Dallas-Fort Worth International Airport (Code: DFW)
+ <ul><li><a href="http://www.dfwairport.com">http://www.dfwairport.com</a></li></ul></li>
+<li>Dallas Love Field (Code: DAL)
+ <ul><li><a href="http://www.dallas-lovefield.com">http://www.dallas-lovefield.com</a></li></ul></li>
+</ul>
+
+<p>Most major U.S. airlines fly to DFW, including American Airlines which
+uses DFW as a major hub. Southwest Airlines is the major carrier at DAL.</p>
+
+<p>Reserving a flight directly to Dallas will provide the most convenient
+access to the conference. Please visit the airport web sites for more
+information.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="shuttle">Ground Transportation</a></h2>
+</div></div>
+<br/>
+
+<p>Numerous shuttles and taxis travel from both airports to downtown Dallas.
+We recommend <a href="http://www.supershuttle.com">SuperShuttle</a> which
+currently has rates of $17 one way ($32 round trip) if you book online.
+(NOTE: Make sure you select Sheraton Dallas at 400 North Olive Street if
+you are staying at the conference hotel. There are 5 Sheraton hotels in
+the Dallas metro area.) Look online for discount codes to reduce your fare
+to $14 each way.</p>
+
+<p>Visit the airport websites for additional ground transportation options.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="rentals">Rail Service</a></h2>
+</div></div>
+<br/>
+
+<p>Within Dallas, the Sheraton Dallas adjacent to the Red and Blue lines of
+the DART Light Rail System. Exit at the Pearl Street station and walk one
+block to North Olive Street. Access to the DART Light Rail is available
+every day except Sundays via the TRE (Trinity Rail Express) for residents
+in the Dallas-Fort Worth metro area. Transfer to the DART at Union
+Station. Visit <a href="http://www.dart.org/">DART</a> and <a href="http://www.trinityrailwayexpress.org/">TRE</a> for more information.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="parking">Driving and Parking at the Sheraton Dallas</a></h2>
+</div></div>
+<br/>
+
+<p>The Sheraton Dallas is in the heart of downtown Dallas, minutes from the
+intersection of interstates 30, 35E, and 45. <em>See map below.</em></p>
+
+<p>Parking at the hotel parking lot costs $19/day for self-parking. There are
+a number of surface lots within a few blocks of the hotel that are
+significantly cheaper.</p>
+
+<p>Visit the <a href="http://www.starwoodhotels.com/sheraton/property/area/transportation.html?propertyID=3207">Sheraton hotel website</a> for more information on transportation options and parking.</p>
+
+<iframe width="725" height="475" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Sheraton+Dallas+Hotel,+400+North+Olive+Street,+Dallas,+TX+75201&amp;sll=37.0625,-95.677068&amp;sspn=38.281301,71.894531&amp;ie=UTF8&amp;hq=Sheraton+Dallas+Hotel,&amp;hnear=400+N+Olive+St,+Dallas,+Texas+75201&amp;ll=32.781718,-96.798134&amp;spn=0.034277,0.062313&amp;z=14&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Sheraton+Dallas+Hotel,+400+North+Olive+Street,+Dallas,+TX+75201&amp;sll=37.0625,-95.677068&amp;sspn=38.281301,71.894531&amp;ie=UTF8&amp;hq=Sheraton+Dallas+Hotel,&amp;hnear=400+N+Olive+St,+Dallas,+Texas+75201&amp;ll=32.781718,-96.798134&amp;spn=0.034277,0.062313&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+            
+<?php include("../parts/middle.php"); ?>  
+
+<?php include("../parts/attendees_right.php"); ?>
+
+    
+<?php
+  $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
+  include("../parts/footer.php");
+ ?>
--- parts/attendees_right.php
+++ parts/attendees_right.php
@@ -13,8 +13,8 @@
  <li><a href="/sigcse2011/attendees/special_projects.php"><img src="/sigcse2011/images/bricks.png" align="absmiddle"/> SIGCSE Special Projects</a></li>
  <li><a href="/sigcse2011/attendees/video_program.php"><img src="/sigcse2011/images/television.png" align="absmiddle"/> Video Program</a></li>
  <li><a href="/sigcse2011/attendees/k12_highlights.php"><img src="/sigcse2011/images/house.png" align="absmiddle"/> Highlights for K-12 Teachers</a></li>
- <li><a href="/sigcse2011/attendees/travel_information.php"><img src="/sigcse2011/images/tag_orange.png" align="absmiddle"/> Travel Information</a></li>
 -->
+ <li><a href="/sigcse2011/attendees/travel_information.php"><img src="/sigcse2011/images/tag_orange.png" align="absmiddle"/> Travel Information</a></li>
  <li><a href="/sigcse2011/attendees/accommodations.php"><img src="/sigcse2011/images/clock.png" align="absmiddle"/> Accommodations</a></li>
 <!--
  <li><a href="/sigcse2011/attendees/floor_plans.php"><img src="/sigcse2011/images/map.png" align="absmiddle"/> Floor Plans</a></li>
