35d374097feb40e0a3f44e0e1884b7770ed99078
Mike Helmick <mike.helmick@gmail.com>
2010-10-28 18:20:06 -0700

    update accomidations page, add hotel image, info about wireless.

--- attendees/accommodations.php
+++ attendees/accommodations.php
@@ -5,21 +5,36 @@
 ?>       
      
 	         <!-- right area -->
-<h1>SIGCSE 2010 Accommodations</h1>
+<h1>SIGCSE 2011 Accommodations</h1>
 
-<p>To help conference attendees begin their travel planning, we are happy to provide the following information about the hotels which will have blocks of rooms available at special conference rates. And, as SIGCSE has done in the past, we have a roommate matching website to help pair you with a roommate to reduce the cost of attending the symposium.</p>
+<p>To help conference attendees begin their travel planning, we are happy to
+provide the following information about the hotel which will have blocks
+of rooms available at special conference rates. And, as SIGCSE has done in
+the past, we will have a roommate matching website to help pair you with a
+roommate to reduce the cost of attending the symposium.</p>
 
 <div class="SectionHeader"><div class="Full">
 <h2><a name="need">Hotel</a></h2>
 </div></div>
 <br/>
 
-<p>The conference hotel is Sheraton Dallas Hotel, 400 North Olive Street, Dallas, TX 75201. The group rate is $159 + tax.<br/> <em>Reservation information will be posted when it is available.</em></p>
+<p><img src="/sigcse2011/images/sheraton-dallas-hotel.jpg" width="300" align="left" class="headshot" />
+The conference hotel is Sheraton Dallas Hotel, 400 North Olive Street, Dallas, TX 75201. The group rate is $159 + tax.  This rate includes free wireless
+access in hotel rooms in the Sheraton.<br/> <em>Reservation information will be posted when it is available.</em>
+<br/>&nbsp;<br/>
+<strong>All conference activities will be at the Sheraton hotel and its attached conference center.</strong>
+<br/>
+<em><a href="travel_information.php">View a map and see travel information.</a></em>
+</p>
 
-<p><strong>All conference activities will be at the Sheraton hotel and its attached conference center.</strong></p>
 
-<iframe width="725" height="475" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Sheraton+Dallas+Hotel,+400+North+Olive+Street,+Dallas,+TX+75201&amp;sll=37.0625,-95.677068&amp;sspn=38.281301,71.894531&amp;ie=UTF8&amp;hq=Sheraton+Dallas+Hotel,&amp;hnear=400+N+Olive+St,+Dallas,+Texas+75201&amp;ll=32.781718,-96.798134&amp;spn=0.034277,0.062313&amp;z=14&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Sheraton+Dallas+Hotel,+400+North+Olive+Street,+Dallas,+TX+75201&amp;sll=37.0625,-95.677068&amp;sspn=38.281301,71.894531&amp;ie=UTF8&amp;hq=Sheraton+Dallas+Hotel,&amp;hnear=400+N+Olive+St,+Dallas,+Texas+75201&amp;ll=32.781718,-96.798134&amp;spn=0.034277,0.062313&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
             
+<div class="SectionHeader" style="clear: left"><div class="Full">
+<h2><a name="need">Roommate matching</a></h2>
+</div></div>
+
+<p><em>Roommate matching information will be posted soon.</em></p>
+
 <?php include("../parts/middle.php"); ?>  
 
 <?php include("../parts/attendees_right.php"); ?>
