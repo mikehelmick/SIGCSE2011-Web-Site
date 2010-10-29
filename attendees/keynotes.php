cec4c2b3d7dd452679db31221697ac20249f60b7
Mike Helmick <mike.helmick@gmail.com>
2010-10-28 18:33:53 -0700

    updated information for the keynote speakers page

--- attendees/keynotes.php
+++ attendees/keynotes.php
@@ -8,12 +8,70 @@
 	         <!-- right area -->
 <h1>SIGCSE 2011 Keynote Speakers</h1>
 
+<ul>
+ <li><a href="#thursday">Matthias Felleisen</a> (Thursday)</li>
+ <li><a href="#friday">Susan Landau</a> (Friday)</li>
+ <li><a href="#saturday">Luis Von Ahn</a> (Saturday)</li>
+</ul>
+
 <div class="SectionHeader"><div class="Full">
-<h2><a name="need">Susan Landau</a></h2>
+<h2><a name="thursday">Matthias Felleisen</a></h2>
+</div></div>
+<p>Northeastern University,
+Winner of the 2011 SIGCSE Award for Outstanding Contribution to Computer
+Science Education</p>
+
+<h3>TeachScheme!</h3>
+
+<p><em>Thursday, March 10, 2011, 8:30-10:00AM</em></p>
+
+<p><img src="/sigcse2011/attendees/speakers/felleisen.jpg" width="170" align="left" class="headshot" />
+	In 1995, my research team and I decided to create TeachScheme!, an
+	educational outreach project, with the hope that our work on programming
+	languages could effect a dramatic change in K-12 computer science.
+	Specifically, we envisioned a virtuous cycle of two mutually reinforcing
+	ideas. On the one hand, we would create a design-oriented curriculum path
+	from middle school through college. On the other hand, our approach would
+	help kids with learning school mathematics. Hence a course on programming
+	would benefit every student, not just those who end up choosing computer
+	science as a college major. At this point, we have a new design-oriented
+	curriculum; a pedagogic program development environment to make it fun;
+	and a series of matching programming languages. After focusing at the
+	overlap between high schools and colleges at first, we now use
+	after-school programs to move upstream, and we are working on two major
+	downstream courses for the second semester in college: one on
+	object-oriented design and another on logic in program design.<br/>&nbsp;<br/>
+	
+	My talk will focus on just one aspect of the project: the design-oriented
+	curriculum and its smooth path from middle school to college.  I will
+	first demonstrate how to teach an intellectually interesting and fun
+	course on programming with something that looks like plain school
+	mathematics. For the rest of the talk, I will sketch the path from there
+	through college.</p>
+	
+<p><em>Bio:</em> 	Matthias Felleisen obtained his PhD ('87) from Daniel P. Friedman who also
+	pointed him in the direction of a professorial career. He then spent the
+	next 15 years at Rice University in Houston, including long and short
+	sabbaticals at Carnegie Mellon University (Pittsburgh) and Ecole Normale
+	Superieure (Paris). In 2001, he took on a position at Northeastern
+	University in Boston and moved his entire team there. Felleisen and his
+	distributed PLT team conduct research on all aspects of programming
+	languages: design, implementations, and applications. On the side, they
+	also run TeachScheme!, an educational outreach project.  Over his 25-year
+	career, Felleisen co-authored six books. As a PhD student, he revised his
+	adviser's "Little LISPer" (MIT Press), which is still in print in its 35th
+	 year of existence. The two of them also wrote "A Little Java, A Few
+	Patterns". With some of his own PhD students, Felleisen produced "How to
+	Design Programs" (MITP, 2001) and "Semantics Engineering" (MITP, 2009).</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="friday">Susan Landau</a></h2>
 </div></div>
 <br/>
 
 <h3>A Computer Scientist Goes to Washington:  How to be Effective in a World Where Facts are 10% of the Equation</h3>
+
+<p><em>Friday, March 11, 2011, 8:30-10:00AM</em></p>
 
 <p>Government's role in computer science is much larger than funding agencies.   Digital rights management, net neutrality and cybersecurity are hot topics in Washington, hot topics where regulation or legislation may have major impact on the computer systems we develop and enjoy.   Yet the rules governing DC are very different than the rules that govern science and engineering, and learning how to operate in a world where facts are only ten percent of the equation can be a challenging experience for someone more accustomed to proving theorems and building systems.   I'll describe what it takes for a nerd to be effective in the world of government, and give some specific examples in the hot area of cyberwar.</p>
 
@@ -40,12 +98,36 @@ Cornell, and her BA from Princeton.</p>
 
 
 <div class="SectionHeader" style="clear:left;"><div class="Full">
-<h2><a name="need">Luis Von Ahn</a></h2>
+<h2><a name="saturday">Luis Von Ahn</a></h2>
 </div></div>
+
+<h3>Three Human Computation Projects</h3>
+
+<p><em>Saturday, March 12, 2011, 12:30-2:30PM</em></p>
 
 <p>
 <img src="/sigcse2011/attendees/speakers/von_Ahn_Luis.jpg" width="180" align="left" class="headshot" />
-	
+At the height of its construction, 44,733 people worked on the Panama
+Canal. The Great Pyramid of Giza required 50,000 workers and the
+Apollo Project 400,000. No matter what you put on this list,
+humanity’s largest achievements have been accomplished with less than
+a few hundred thousand workers because it has been impossible to
+assemble (let alone pay!) more people to work together--until now.
+With the Internet, we can coordinate the efforts of billions of
+humans. If 400,000 people put a man on the moon, what can we do with
+100 million? My research aims to develop theories and build computer
+systems that enable massive collaborations between humans and
+computers for the benefit of humanity. I am working to develop a new
+area of computer science called human computation, which studies how
+to harness the combined power of humans and computers to solve
+problems that would be impossible for either to solve alone.
+<br/>&nbsp;<br/>
+In this talk I will discuss several examples of my work in human
+computation, including reCAPTCHA, The ESP Game, and Duolingo.
+I have used these projects in many of my courses to illustrate state
+of the art concepts in computer science.</p>
+
+<p><em>Bio:</em>	
 	Professor Luis von Ahn works in the Computer Science Department at
 	Carnegie Mellon University. He also works for Google after the
 	acquisition of his company reCAPTCHA. His current research interests
