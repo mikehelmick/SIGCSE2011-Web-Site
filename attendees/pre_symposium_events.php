e048fdc620756b1b7598eade6e228715ba2b4af8
Mike Helmick <mike.helmick@gmail.com>
2010-11-18 23:32:08 -0800

    add pre symposium events page

---
+++ attendees/pre_symposium_events.php
@@ -0,0 +1,141 @@
+<?php
+ $title = "SIGCSE 2011 - Pre-Symposium Events";
+ $menu = "attendees";
+ include("../parts/top.php");
+?>       
+<h1>SIGCSE 2011 Pre-Symposium Events</h1>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="csta">CSTA Source Web Repository Cataloging Workshop</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am to 3:00pm.	
+
+<p>Chris Stephenson, CSTA Executive Director</p>
+	
+<p>Contact: Christina San Filippo <script>write_email("sanfilippo", "hq.acm.org");</script></p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="department_chairs">Department Chairs' Pre-Symposium Workshop</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, TBA
+<p>The Pre-Symposium Workshop for Department Chairs is an opportunity for all department chairs (experienced and inexperienced) to improve their understanding of their duties and their ability to perform them.  Participants should also include those interested in becoming department chairs.  Four experienced former department chairs will serve as resources for the group as the participants review and discuss the duties of the department chair and explore ways that chairs can do their jobs efficiently and effectively.  Some specific discussion topics will include leadership styles, time management, legal issues, academic program assessment, and faculty promotion and tenure.
+</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="innovativeApproaches">Innovative approaches to introducing Computer Science</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 11:00am - 5:30pm
+<p>More information at <a href="http://harambeenet.org/sigcse11">http://harambeenet.org/sigcse11</a></p>
+<p>Organizers and affiliations: 
+<ul>
+ <li>Jeff Forbes, Duke University - <script>write_email("jforbes", "duke.edu");</script></li>
+ <li>Owen Astrachan, Duke University - <script>write_email("ola", "cs.duke.edu");</script></li>	
+</ul>
+</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="womenFaculty">Managing the Academic Career for Women Faculty in Undergraduate Computing Programs</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am - 5:30pm
+<p>More information at <a href="http://cra-w.org/mentorWrkshp/cmwe-2011">http://cra-w.org/mentorWrkshp/cmwe-2011</a></p>
+<p>
+<ul>
+ <li>Susan Rodger, Duke University</li>
+ <li>Sheila Castaneda, Clarke University - <script>write_email("Sheila.Castaneda", "clarke.ed");</script></li>
+</ul>
+</p>
+
+
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="new_faculty">New Educators Roundtable</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, TBA
+
+<p>New to teaching? Seeking advice and mentorship? Join us at the New Educators Roundtable (NER), a half-day workshop designed to mentor college and university faculty who are new to teaching. We welcome participants from across the entire teaching spectrum: tenure-track faculty at institutions where teaching is central, teaching-track faculty and lecturers, and graduate students who currently teach or are looking forward to an academic career. Come with your questions and concerns, learn from the experiences and best practices of sage elders, and build an international support group for your career. The roundtable will be held from (time to be determined) on Wednesday, March 9, 2010, immediately before the SIGCSE Symposium. Lunch will be provided.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="sigcas">Open meeting of the Special Interest Group on Computers and Society (SIGCAS)</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am - 12:00pm
+<p>This open meeting of ACM’s Special Interest Group on Computers and Society (SIGCAS) is
+an opportunity for SIGCAS members and supporters to provide input into the future direction
+of the SIG. Particular attention will be paid to issues of interest to SIGCAS members who are
+primarily computer science educators.</p>
+<p>Organizers and affiliations: SIGCAS, Florence Appel, Saint Xavier University</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="programByDesign">Program by Design: Bootstrap, TeachScheme, ReachJava, and Beyond</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am to 5:30pm
+<p>More information at <a href="http://www.programbydesign.org/events/sigcse2011/">http://www.programbydesign.org/events/sigcse2011/</a></p>
+<p>Contact: Viera Proulx, <script>write_email("vkp", "ccs.neu.edu");</script></p>
+
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="alice2.2">Programming with Alice Workshop (Alice 2.2)</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am - 5:30pm	
+<p>More information at <a href="http://www.andrew.cmu.edu/user/wpdann/workshops.html">http://www.andrew.cmu.edu/user/wpdann/workshops.html</a></p>
+<p>Contact: Wanda Dann, Carnegie-Mellon University<br/>
+<script>write_email("wpdann", "andrew.cmu.edu");</script>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="alice3">Programming with Alice Workshop (Alice 3)</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am - 5:30pm	
+<p>More information at <a href="http://www.andrew.cmu.edu/user/wpdann/workshops.html">http://www.andrew.cmu.edu/user/wpdann/workshops.html</a></p>
+<p>Contact: Wanda Dann, Carnegie-Mellon University<br/>
+<script>write_email("wpdann", "andrew.cmu.edu");</script>
+
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="ethics">Teaching Computer Ethics in the Computer Science Curriculum</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 1:15pm to 5:30pm	
+<p>More information at <a href="https://edocs.uis.edu/kmill2/www/sigcse2011computerEthics.html">https://edocs.uis.edu/kmill2/www/sigcse2011computerEthics.html</a></p>
+<p>Organizers and affiliations:
+<ul>
+ <li>Bo Brinkman, Miami University (Ohio)</li>
+ <li>Don Gotterbarn, East Tennessee State University - <script>write_email("don", "gotterbarn.com");</script></li>
+ <li>Keith Miller, University of Illinois Springfield</li>
+</ul>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="sigcas">The 3rd Annual Humanitarian FOSS Symposium 2011</a></h2>
+</div></div>
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, TBA
+
+<p>The Third annual HFOSS Education Symposium (<a href="http://www.hfoss.org/hfoss2011">HFOSS 2011</a>), held as
+a SIGCSE 2011 pre-conference activity (March 9th 2011, Dallas, Texas), aims to bring together educators,
+software developers, industry representatives, and students to continue the discussion of how best to
+promote HFOSS within undergraduate computing education. Humanitarian FOSS is free and open source
+software that contributes in some way to the public good. This year’s symposium focuses on applying
+HFOSS within the local community -- i.e., in local government, in K-12 education, and in collaboration
+with local non-profit organizations. This years keynote speaker is Bryan Sivak, Chief Technology Officer
+for the District of Columbia, founding member and advisory board member of Civic Commons. As in
+previous years, the symposium will be highly interactive, taking place entirely in plenary sessions. In
+addition to the keynote address by Bryan Sivak, the symposium will include a poster session, and panels
+on government, K-12 education, and non-profits, made up of invited speakers and participants from
+industry, academia, and the non-profit sector.</p>
+<p>Organizers and affiliations:
+<ul>
+<li>Ralph Morelli (chair), Trinity College</li>
+<li>Trishan de Lanerolle, Trinity College</li>
+<li>Danny Krizanc, Wesleyan University</li>
+<li>Norman Danner, Wesleyan University</li>
+<li>Gary Parker, Connecticut College</li>
+<li>Ozgur Izmirli, Connecticut College</li>
+<li>Carlos Espinosa, Trinity College</li>
+<li>Allen Tucker, Bowdoin College </li>
+</ul>
+</p>
+
+<br/>
+
+<?php include("../parts/middle.php"); ?>  
+<?php include("../parts/attendees_right.php"); ?>
+<?php
+  $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
+  include("../parts/footer.php");
+ ?>
