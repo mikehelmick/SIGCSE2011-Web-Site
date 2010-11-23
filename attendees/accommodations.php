16c4993708ac949236e4de58ebba1c04564c613f
Mike Helmick <mike.helmick@gmail.com>
2010-11-22 18:26:05 -0800

    Added hotel reservations link

--- attendees/accommodations.php
+++ attendees/accommodations.php
@@ -14,23 +14,44 @@ the past, we will have a roommate matching website to help pair you with a
 roommate to reduce the cost of attending the symposium.</p>
 
 <div class="SectionHeader"><div class="Full">
-<h2><a name="need">Hotel</a></h2>
+<h2>Conference Hotel</h2>
 </div></div>
 <br/>
 
 <p><img src="/sigcse2011/images/sheraton-dallas-hotel.jpg" width="300" align="left" class="headshot" />
 The conference hotel is Sheraton Dallas Hotel, 400 North Olive Street, Dallas, TX 75201. The group rate is $159 + tax.  This rate includes free wireless
-access in hotel rooms in the Sheraton.<br/> <em>Reservation information will be posted when it is available.</em>
+access in hotel rooms in the Sheraton.<br/> 
+<b><a href="http://www.starwoodmeeting.com/StarGroupsWeb/res?id=1011221675&key=112A2">Click here to make your hotel reservations</a>
 <br/>&nbsp;<br/>
 <strong>All conference activities will be at the Sheraton hotel and its attached conference center.</strong>
 <br/>
 <em><a href="travel_information.php">View a map and see travel information.</a></em>
 </p>
 
+<div class="SectionHeader" style="clear: left;"><div class="Full">
+<h2>Why should I stay at the conference hotel?</h2>
+</div></div>
+
+<p>If you stay at the conference hotel, you’ll be able to network easily with
+other conference attendees.  It’s a quick trip to get to the program and
+functions, and just as quick to zip back to your room for a break.  Plus,
+we've negotiated for free Internet at the conference hotel and a
+significantly reduced room rate to stay in downtown Dallas. Overall,
+you’ll have a much better conference experience if you stay at the
+conference hotel. Staying at the conference hotel also supports SIGCSE
+since we are required to pay for unused rooms in our reserved block which
+raises costs for future SIGCSE conferences. So make your conference
+experience a better one and help SIGCSE at the same time --- please
+reserve your room at the Sheraton Dallas.</p>
+
+<p><em>(If you somehow obtain a better rate online than the one listed on our
+site, your stay will still be counted toward our required room block, but
+keep in mind that there are often restrictions and/or penalties associated
+with these rates. )</em></p>
 
             
 <div class="SectionHeader" style="clear: left"><div class="Full">
-<h2><a name="need">Roommate matching</a></h2>
+<h2>Roommate matching</h2>
 </div></div>
 
 <p><em>Roommate matching information will be posted soon.</em></p>
