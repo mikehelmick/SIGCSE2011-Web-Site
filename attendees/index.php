af7c415d124a188df46b0882c26bc4e9969f96aa
Mike Helmick <mike.helmick@gmail.com>
2010-12-02 20:37:33 -0800

    add robot hoedown to right menu

--- attendees/index.php
+++ attendees/index.php
@@ -16,7 +16,7 @@
 	
 	 <ul>
 	 <li><a href="http://db.grinnell.edu/sigcse/sigcse2011/Program/Program.asp"><img src="/sigcse2011/images/report.png" align="absmiddle" /> Symposium Program</a></li>
-	 <li><a href="http://elvis.rowan.edu/~kay/sigcse/rodeo.html">SIGCSE 2011 Robot Hoedown and Rodeo</a>
+	 <li><a href="http://elvis.rowan.edu/~kay/sigcse/rodeo.html"><img src="/sigcse2011/images/bricks.png" align="absmiddle"/> SIGCSE 2011 Robot Hoedown and Rodeo</a> <strong>New!</strong>
 		<ul><li><a href="http://elvis.rowan.edu/~kay/sigcse/cfw.html">Call for Wranglers</a></li></ul>
 	 </li>
 	 <li><a href="/sigcse2011/attendees/keynotes.php"><img src="/sigcse2011/images/user_suit.png" align="absmiddle" /> Keynote Speakers</a></li>
