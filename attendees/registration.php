d17051ee8a015bf15a35b2460a8e62bc2bc2bf49
Mike Helmick <mike.helmick@gmail.com>
2010-12-04 21:40:08 -0800

    added supporter sessions page

--- attendees/registration.php
+++ attendees/registration.php
@@ -6,11 +6,16 @@
      
 <div id="GoodMessage">Registration is not yet open.</div>
 
+<h1>SIGCSE 2011 Registration Gateway</h1>
+      
+<div class="SectionHeader"><div class="Full">
+<h2>Registration</h2>
+</div></div>
+<br/>   
+
+
 <?php include("../parts/middle.php"); ?>  
-
 <?php include("../parts/attendees_right.php"); ?>
-
-    
 <?php
   $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
   include("../parts/footer.php");
---
+++ attendees/supporter_sessions.php
@@ -0,0 +1,71 @@
+<?php
+ $title = "SIGCSE 2011 - Supporter Sessions";
+ $menu = "attendees";
+ include("../parts/top.php");
+?>
+<h1>SIGCSE 2011 Supporter Sessions</h1>
+     
+<div class="SectionHeader" style="clear:left;"><div class="Full">
+<h2>SIGCSE 2011 Supporter Sessions</h2>
+</div></div>
+
+<ul>
+ <li><strong>Thursday, March 10, 2011</strong><ul>
+   <li>10:45 - 12:00 - <a href="#microsoft">Mircrosoft</a></li>
+   <li>1:45 - 3:00 - <a href="#intel">Intel</a></li>
+   <li>3:45 - 5:00 - <a href="#ibm">IBM</a></li>
+ </ul></li>
+ <li><strong>Friday, March 11, 2011</strong><ul>
+   <li>10:45 – 12:00 - <a href="#microsoft">Mircrosoft</a></li>
+   <li>1:45 – 3:00 - <a href="#intel">Intel</a></li>
+   <li>3:45 – 5:00</li>
+ </ul></li>
+ <li><strong>Saturday, March 12, 2011</strong><ul>
+   <li>10:55 - 12:10</li>
+ </ul></li>
+</ul>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="intel">Intel</a></h2>
+</div></div>
+<p><ul><li>Thursday, March 10, 2011 - 1:45pm to 3:00pm</li>
+<li>Friday, March 11, 2011 - 1:45pm to 3:00pm</li></ul></p>
+<div class="imgRight"><img src="/sigcse2011/images/logos/sponsors/intel.png" /></div>
+<p>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="microsoft">Microsoft</a></h2>
+</div></div>
+
+<p><ul><li>Thursday, March 10, 2011 - 10:45am to 12:00pm</li>
+<li>Friday, March 11, 2011 - 10:45am to 12:00pm</li></p>
+<div class="imgRight"><img src="/sigcse2011/images/logos/sponsors/microsoft.png" /></div>
+<p>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="ibm">IBM</a></h2>
+</div></div>
+
+<p>Thursday, March 10, 2011: 3:45pm to 5:00pm</p>
+<div class="imgRight"><img src="/sigcse2011/images/logos/sponsors/ibm.png" /></div>
+
+<p><strong>Can Students Really Develop Software Collaboratively?</strong><br/>Speaker: Brian Schimpf</p>
+<p>In the "real world" of software development, developers rarely work on a project alone.
+Development has become a "team sport" with accepted rules, best practices, and preferred
+tools. However, too often, students graduate only knowing how to develop code as an individual
+– not as part of a team. With IBM's Rational Team Concert (free for classroom use from IBM's
+Academic Initiative) students can work in a real-world team development scenario -- each with
+different roles on the team. Work items can be sized and assigned to each team member,
+and anyone can view real-time status and project health. Even better, professors can use the
+dashboards and reports to view each student's contribution to the project to help them assess
+each student individually.</p>
+
+
+
+
+<?php include("../parts/middle.php"); ?>  
+<?php include("../parts/attendees_right.php"); ?>
+<?php
+  $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
+  include("../parts/footer.php");
+ ?>
