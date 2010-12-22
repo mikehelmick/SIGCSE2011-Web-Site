1bd563018a972d54eec75e28bf907d3ef9d4f987
Mike Helmick <mike.helmick@gmail.com>
2010-12-21 22:56:36 -0800

    add kids camp FAQ

--- attendees/kids_camp.php
+++ attendees/kids_camp.php
@@ -36,6 +36,31 @@
 <li>Full day: $56 per child</li>
 </ul></li></ul>
 
+<h3>Frequently Asked Questions</h3>
+<ol>
+<li><strong>How old/young can my child(ren) be to register?</strong>
+<p>Care will be provided for children ages 6 months to 12 yrs. If you have older children who want to attend, please contact us.</p></li>
+<li><strong>How much does it cost?</strong>
+<p>Great news: our rates are unchanged this year! Like last year the charges are by session:<br/>
+<strong>Children 6 months - 2 years:</strong> $35 per child (half day), or $56 per child (full day)<br/>
+<strong>Children 3 - 12 years:</strong> $30 per child (half day), or $48 per child (full day)</p></li>
+<li><strong>Where will Kids Camp be located?</strong>
+<p>Kids Camp is conveniently located in the Remington Room on the fourth floor of the Sheraton Hotel, directly above most conference activities.  Visiting your children will be easy, yet they are comfortably away from the main conference traffic. Restrooms are located nearby.  You can check out the room here:  <a href="http://www.sheratondallashotel.com/capacity-chart">http://www.sheratondallashotel.com/capacity-chart</a>).</p></li>
+<li><strong>What will my child(ren) be doing while I am attending the conference?</strong>
+<p>SIGCSE Kid Campers will participate in drama, play, and arts & crafts activities. Older kids can participate in our planned computing activities. We have scheduled activity sessions with CS Unplugged, Scratch, and Alice. Student Volunteers will help with the computing activities.</p>
+<p>Our planned activities also include:
+<ul><li>Computing activities using Microsoft's Kodu</li>
+<li>Scavenger hunts on both Thursday and Friday</li>
+<li>Daily activites based on our "Going Green!" theme</li>
+<li>...and much more!</li></ul></p></li>
+<li><strong>What will my children be served for lunch?</strong>
+<p>Lunch menu selections are still in planning. Last year's camp menu included entrees of chicken fingers & fries, grilled cheese & fries, macaroni & cheese, and pizza (with 1 topping). Side dishes were celery & carrot sticks, fruit cup, and a selection of whole fruit (apple, banana, orange).</p></li>
+<li><strong>Do I need to bring anything for my child(ren)?</strong>
+<p>Yes, a change of clothes is strongly recommended for all children. In addition, parents of young children will need to provide any diapers, wipes, baby food, and/or formula and bottles that may be needed.</p></li>
+<li><strong>How many childcare providers will be in the rooms?</strong>
+<p>Daycare provider ratios will be 1:2 for kids ages 6 months-11 months, 1:3 for kids 1 to 2 years old, 1:5 for ages 3-5 and 1:7 for 6-12 years old.. Student volunteers for the computing activities will be additional helpers and are not included in this ratio.</p></li>
+</ol>
+
 <?php include("../parts/middle.php"); ?>  
 <?php include("../parts/attendees_right.php"); ?>
 <?php
