c39e1821986cf72a615071578007e09f774e3f5c
Mike Helmick <mike.helmick@gmail.com>
2010-12-21 23:34:15 -0800

    add intel supporter sessions

--- attendees/supporter_sessions.php
+++ attendees/supporter_sessions.php
@@ -30,8 +30,40 @@
 </div></div>
 <p><ul><li>Thursday, March 10, 2011 - 1:45pm to 3:00pm</li>
 <li>Friday, March 11, 2011 - 1:45pm to 3:00pm</li></ul></p>
-<div class="imgRight"><img src="/sigcse2011/images/logos/sponsors/intel.png" /></div>
-<p>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;</p>
+
+<p>
+<img src="/sigcse2011/images/logos/sponsors/intel.png" align="left" class="headshot" />	
+	
+<strong>Intel Academic Community – Global MeeGo</strong><br/>
+Speaker: Selwyn You - Intel Corporation, Jukka Heikkilä - University of Jyväskylä, Russel J Clark and Matthew Wolf - Georgia Institute of Technology<br/>
+<em>Thursday, March 10: 1:45-3:00 PM</em><br/>&nbsp;<br/>
+Have you heard the buzz about MeeGo? The newly formed MeeGo Operating System, a combination of Intel’s Moblin and Nokia’s Maemo, provides a unified development environment for developers with worldwide distribution channels to consumers. Learn how Intel’s Global MeeGo University program can help
+	you integrate application development on netbooks, tablets,
+	and smartphones into your curricula. Professors from the
+	University of Jyväskylä and Georgia Institute of Technology will
+	share insight into classroom modules that meet demand for rich
+	application development on client devices. They will also share
+	student feedback, in addition to how, why, and when you should
+	start introducing MeeGo to your students.</p>
+
+<p><strong>Case Study - Using the Intel Manycore Testing Lab to Test and Scale Applications</strong><br/>
+Speaker Name: Bob Chesebrough, Innovative Software Education, Intel<br/>
+<em>Friday March 11 - 1:45 pm- 3:00 pm</em><br/>&nbsp;<br/>
+	Expand your students’ learning experiences with the Intel Manycore Testing Lab—a
+	unique, global, remote-access facility made available at no charge to members of the
+	Intel Academic Community. The Intel Manycore Testing lab can be used to test, validate,
+	and improve the scalability of classroom labs, homework, and capstone projects. The
+	lab supports both Linux* and MS Windows* with a 32-CPU/64-thread development
+	environment, including up-to-date, essential performance tools to assist professors and
+	their students.</p>
+<p>
+	Bob Chesebrough will demonstrate a command-line ray tracer application created by
+	Master’s degree student Dave Frogley. The application was tested, scaled, and tuned
+	on the Intel Manycore Testing Lab. Discussions will include scaling methodologies and
+	state-of-the-art software tools. Bob Chesebrough is a senior course architect in Intel’s
+	Innovative Software Education department.	
+</p>
+	
 
 <div class="SectionHeader"><div class="Full">
 <h2><a name="microsoft">Microsoft</a></h2>
@@ -47,10 +79,12 @@
 </div></div>
 
 <p>Thursday, March 10, 2011: 3:45pm to 5:00pm</p>
-<div class="imgRight"><img src="/sigcse2011/images/logos/sponsors/ibm.png" /></div>
 
-<p><strong>Can Students Really Develop Software Collaboratively?</strong><br/>Speaker: Brian Schimpf</p>
-<p>In the "real world" of software development, developers rarely work on a project alone.
+<p>
+<img src="/sigcse2011/images/logos/sponsors/ibm.png" align="left" class="headshot" />	
+<strong>Can Students Really Develop Software Collaboratively?</strong><br/>Speaker: Brian Schimpf<br/>
+&nbsp;</br/>
+In the "real world" of software development, developers rarely work on a project alone.
 Development has become a "team sport" with accepted rules, best practices, and preferred
 tools. However, too often, students graduate only knowing how to develop code as an individual
 – not as part of a team. With IBM's Rational Team Concert (free for classroom use from IBM's
