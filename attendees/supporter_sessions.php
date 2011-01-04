11e6bd93f3e5606c9655923f5ad78bb28ee4f231
Mike Helmick <mike.helmick@gmail.com>
2011-01-03 22:49:27 -0800

    add microsoft supporter sessions

--- attendees/supporter_sessions.php
+++ attendees/supporter_sessions.php
@@ -71,8 +71,30 @@ Speaker Name: Bob Chesebrough, Innovative Software Education, Intel<br/>
 
 <p><ul><li>Thursday, March 10, 2011 - 10:45am to 12:00pm</li>
 <li>Friday, March 11, 2011 - 10:45am to 12:00pm</li></p>
-<div class="imgRight"><img src="/sigcse2011/images/logos/sponsors/microsoft.png" /></div>
-<p>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;</p>
+
+<p>
+<img src="/sigcse2011/images/logos/sponsors/microsoft.png" align="left" class="headshot" />
+
+<strong>F# - From Foundations to Modern Parallel and Data Rich Functional Programming</strong><br/>
+Speakers: Don Syme (Microsoft Research, Cambridge) and Nigel Horspool tbc (University of Victoria)<br/>
+<em>Thursday, March 10, 2011 - 10:45am to 12:00pm<br/>
+&nbsp;<br/>
+F# is a multi-paradigm programming language, targeting the .NET/Mono platforms. F# brings you type safe, succinct, efficient and expressive functional programming language supporting both the imperative and object-oriented programming disciplines. It is a simple and pragmatic language, and has particular strengths in data-oriented programming, parallel I/O programming, parallel CPU programming, scripting and algorithmic development. F# combines the advantages of typed functional programming with a high-quality, well-supported modern runtime system and set of libraries. This tutorial will teach F# from several angles: 
+<ul>
+<li>F# as a cross platform language for teaching, including using F# on Windows, Macs, with Linux and MonoDevelop, and on the TryF# system</li>
+<li>The advanced features of F#, including asynchronous and parallel programming and units of measure</li>
+<li>F# type providers and their applications to strongly typed programming with web ontologies</li>
+<li>F# as a modern data programming environment, including writing data-rich phone applications</li>
+</ul>
+Attendees should come away with a good idea of the utility and power of F# in the classroom context, and its use all the way up to postgraduate and research level.</p>
+
+<p>
+<strong>PexForFun – Programming Exercises and Automatic Grading in the Cloud</strong><br/>
+Speakers: Peli de Halleux and Nikolai Tillmann, Microsoft Research, Redmond<br/>
+<em>Friday, March 11, 2011 - 10:45am to 12:00pm</em><br/>&nbsp;<br/>
+PexForFun can be used to learn software programming at many levels, from high school all the way through graduate courses. With PexForFun, the student edits the code in any browser and we execute it and analyze it for you in the cloud. PexForFun supports C#, VisualBasic and F#.  Because Pex4Fun has Intellisense, learning the syntax is not an obstacle for beginners. PexForFun finds interesting and unexpected input values that help students understand what their code is actually doing. Under the hood, PexForFun uses dynamic symbolic execution to thoroughly explore feasible execution paths. The real fun starts with Coding Duels where the student has to write code that implements a specification. PexForFun finds any discrepancies between the student’s code and the specification. PexForFun connects teachers, curriculum authors and students in a unique social experience, tracking and streaming progress updates in real time. Enough said, try it for yourself at <a href="http://pex4fun.com/">http://pex4fun.com/</a>  and come and hear about it at this tutorial, given by the authors of the system.	
+</p>
+
 
 <div class="SectionHeader"><div class="Full">
 <h2><a name="ibm">IBM</a></h2>
