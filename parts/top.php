96cd609814c6474a3a464c4fed2a203a67bf8a41
Mike Helmick <mike.helmick@gmail.com>
2011-01-04 21:33:57 -0800

    force header content-type to be UTF-8 encoding

--- parts/top.php
+++ parts/top.php
@@ -1,3 +1,4 @@
+<?php header('Content-type: text/html; charset=utf-8'); ?>
 <!DOCTYPE html 
      PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -5,7 +6,7 @@
 
 <head>
 	<title><?php echo $title ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     
     <meta name="google-site-verification" content="DyK55ZZUCG5OEtmtFwEAvttufQiH92x17UQsiZyrU_w" />
     
