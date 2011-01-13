cbc90ced18cf347db1cffdda1014841c58778fc6
Mike Helmick <mike.helmick@gmail.com>
2011-01-12 22:23:23 -0800

    update google link

--- parts/sponsor_google.php
+++ parts/sponsor_google.php
@@ -1,3 +1,3 @@
 <td align="center" valign="middle">
-  <a href="http://research.google.com/university/" alt="Google"><img src="/sigcse2011/images/logos/sponsors/google.png" /></a>
+  <a href="http://www.google.com/edu/index.html" alt="Google"><img src="/sigcse2011/images/logos/sponsors/google.png" /></a>
 </td>
