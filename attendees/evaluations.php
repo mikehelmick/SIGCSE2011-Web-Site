c6427989ce60c5b6420ab43ff99556852e1b7346
Mike Helmick <mike.helmick@gmail.com>
2011-01-17 14:49:59 -0800

    remove workshop survey link for 2011

--- attendees/evaluations.php
+++ attendees/evaluations.php
@@ -10,7 +10,6 @@
 <p>To complete an evaluation on-line, please select one of the following links. Note that neither survey requires a password:
 <ul>
  <li><a href="http://www.surveymonkey.com/s/SIGCSE2011">SIGCSE 2011 Symposium Evaluation</a></li>
- <li><a href="http://www.surveymonkey.com/s/SIGCSE2011Workshops">Wednesday Evening, Friday Evening, and Saturday Workshop Evaluations (to be completed once per workshop attended)</a></li>
 </ul>
 </p>
 <p>Please be aware: If you are unable or unwilling to complete either or both of these surveys on-line, paper copies of the surveys are available from the symposium registration desk at the convention center.</p>
