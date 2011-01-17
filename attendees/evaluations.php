cd5e9f59c9412322c528e442286ddd284566c75c
Mike Helmick <mike.helmick@gmail.com>
2011-01-17 14:59:27 -0800

    update evaluations page

--- attendees/evaluations.php
+++ attendees/evaluations.php
@@ -6,13 +6,14 @@
 <div class="SectionHeader" style="clear:left;"><div class="Full">
 <h2>SIGCSE 2011 Symposium Evaluations</h2>
 </div></div>
-<p>The SIGCSE 2011 Conference Committee hopes that you enjoyed (or are still enjoying!) this year's symposium. To help us learn what we did well, and what we could have done better, we invite you to complete the Symposium Evaluation survey. If you attended one or more of the Wednesday and Friday evening workshops, we would appreciate receiving your feedback on those as well. These survey results not only inform the 2010 committee, they also provide valuable insight to the members of the SIGCSE 2012¡ committee.</p>
-<p>To complete an evaluation on-line, please select one of the following links. Note that neither survey requires a password:
+<p>The SIGCSE 2011 Conference Committee hopes that you enjoyed (or are still enjoying!) this year's symposium. To help us learn what we did well, and what we could have done better, we invite you to complete the Symposium Evaluation survey.  These survey results not only inform the 2011 committee, they also provide valuable insight to the members of the SIGCSE 2012 committee.</p>
+<p>To complete an evaluation on-line, please select one of the following links.
 <ul>
- <li><a href="http://www.surveymonkey.com/s/SIGCSE2011">SIGCSE 2011 Symposium Evaluation</a></li>
+ <li><em>The survey will be available before the conference starts.</em>
+ 	<!-- <a href="http://www.surveymonkey.com/s/SIGCSE2011">SIGCSE 2011 Symposium Evaluation</a> --></li>
 </ul>
 </p>
-<p>Please be aware: If you are unable or unwilling to complete either or both of these surveys on-line, paper copies of the surveys are available from the symposium registration desk at the convention center.</p>
+<p>Please be aware: If you are unable or unwilling to complete this survey on-line, paper copies of the surveys are available from the symposium registration desk at the convention center.</p>
 <p>Thank you!</p>
 <?php include("../parts/middle.php"); ?>  
 <?php include("../parts/attendees_right.php"); ?>
