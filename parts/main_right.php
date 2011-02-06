98cd010870b5226fbdd0c5f09e8b474f834d4728
Mike Helmick <mike.helmick@gmail.com>
2011-02-06 13:28:55 -0800

    updated front page news

--- parts/main_right.php
+++ parts/main_right.php
@@ -1,5 +1,7 @@
 <h2>News</h2>
 <ul>
+  <li>The special <a href="/sigcse2011/attendees/accommodations.php">SIGCSE rate for the Dallas Sheraton</a> is available through February 15.</li>
+  <li>The following workshops have been canceled: #25, #31, #32, #34.</li>
   <li><a href="/sigcse2011/attendees/registration.php">Online registration is now open.</a></li>	
 	
   <li>New event for 2011! <a href="http://elvis.rowan.edu/~kay/sigcse/rodeo.html">Robot Hoedown and Rodeo</a>. No experience necessary, but if you have robots and experience, <a href="http://elvis.rowan.edu/~kay/sigcse/cfw.html">we could use you as a wrangler</a>!</li>
