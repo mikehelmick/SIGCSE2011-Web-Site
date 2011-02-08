2d527e64a48c359b9ddccb8cbb7521ff50a3f3f6
Mike Helmick <mike.helmick@gmail.com>
2011-02-08 07:55:13 -0800

    updated times for wednesday sessions

--- attendees/pre_symposium_events.php
+++ attendees/pre_symposium_events.php
@@ -8,7 +8,7 @@
 <div class="SectionHeader"><div class="Full">
 <h2><a name="csta">CSTA Source Web Repository Cataloging Workshop</a></h2>
 </div></div>
-<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am to 3:00pm.	
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am to 3:30pm.	
 <p>Chris Stephenson, CSTA Executive Director</p>
 <p>Participants will learn how to appropriately review and classify resources submitted for entry in to the
 Source Web Repository.</p>	
@@ -17,7 +17,7 @@ Source Web Repository.</p>
 <div class="SectionHeader"><div class="Full">
 <h2><a name="department_chairs">Department Chairs' Pre-Symposium Workshop</a></h2>
 </div></div>
-<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, TBA
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am to 5:30pm.
 <p>The Pre-Symposium Workshop for Department Chairs is an opportunity for all department chairs (experienced and inexperienced) to improve their understanding of their duties and their ability to perform them.  Participants should also include those interested in becoming department chairs.  Four experienced former department chairs will serve as resources for the group as the participants review and discuss the duties of the department chair and explore ways that chairs can do their jobs efficiently and effectively.  Some specific discussion topics will include leadership styles, time management, legal issues, academic program assessment, and faculty promotion and tenure.
 </p>
 
@@ -96,7 +96,7 @@ skills.</p>
 <div class="SectionHeader"><div class="Full">
 <h2><a name="new_faculty">New Educators Roundtable</a></h2>
 </div></div>
-<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 12:30 - 5:00pm
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 1:00pm - 5:30pm
 <p><em>When registering, this event is available as a single choice or as a combo with another half-day event.</em></p>
 
 <p>New to teaching? Seeking advice and mentorship? Join us at the New Educators Roundtable (NER), a half-day workshop designed to mentor college and university faculty who are new to teaching. We welcome participants from across the entire teaching spectrum: tenure-track faculty at institutions where teaching is central, teaching-track faculty and lecturers, and graduate students who currently teach or are looking forward to an academic career. Come with your questions and concerns, learn from the experiences and best practices of sage elders, and build an international support group for your career.</p>
@@ -175,7 +175,7 @@ current users of the curriculum.</p>
 <div class="SectionHeader"><div class="Full">
 <h2><a name="hfoss">The 3rd Annual Humanitarian FOSS Symposium 2011</a></h2>
 </div></div>
-<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:30am - 6:00pm
+<img src="/sigcse2011/images/clock.png" align="absmiddle" />Wednesday, March 9, 2011, 8:00am - 5:30pm
 
 <p>The Third annual HFOSS Education Symposium (<a href="http://www.hfoss.org/hfoss2011">HFOSS 2011</a>), held as a SIGCSE 2011 pre-conference activity (March 9th 2011, Dallas, Texas), aims to bring together educators,
 software developers, industry representatives, and students to continue the discussion of how best to
