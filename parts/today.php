15868fecdac33eb2e52b59bc5bb0c0f7d0713953
Mike Helmick <mike.helmick@gmail.com>
2011-03-11 13:41:37 -0600

    add saturday highlights

--- parts/today.php
+++ parts/today.php
@@ -5,3 +5,14 @@
 <li><a href="http://upe.acm.org/">UPE national meeting</a>, 2011 Abacus Award Presentation and Talk: Donald Knuth (12:45-1:35pm)</li>
 </ul></p>
 </div>
+
+<div class="GoodMessage">
+<p><strong>Saturday March 12th at SIGCSE 2011</strong><br/><ul>
+<li><a href="http://db.grinnell.edu/sigcse/sigcse2011/Program/programAtAGlance.asp?dateRequested=Saturday">Saturday's full program</a></li>
+<li>Keynote: Luis Von Ahn, 12:30pm (<em>luncheon</em>). <a href="/sigcse2011/attendees/keynotes.php#saturday">Three Human Computation Projects</a> in Dallas BCD</li>
+<li><strong>Room Changes</strong><ul>
+ <li>10:55-12:10  Intro CS: Panoptic Views is moving from Dallas D2	to <strong>Lone Star A1</strong></li>
+ <li>10:55-12:10  Software Design and Development is moving from Dallas D1 to <strong>Dallas A3</strong></li>
+</ul></li>
+</ul></p>
+</div>
