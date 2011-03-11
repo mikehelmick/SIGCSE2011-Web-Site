fd5399207206ab61010b10a06289d518c13db09d
Mike Helmick <mike.helmick@gmail.com>
2011-03-10 21:25:08 -0600

    add highlights for friday

--- parts/today.php
+++ parts/today.php
@@ -1,9 +1,7 @@
 <div class="GoodMessage">
-<p><strong>Thursday March 10th at SIGCSE 2011</strong><br/><ul>
-<li><a href="http://db.grinnell.edu/sigcse/sigcse2011/Program/programAtAGlance.asp?dateRequested=Thursday">Thursday's full program</a></li>
-<li>Keynote: Matthias Felleisen, 8:30am. <a href="/sigcse2011/attendees/keynotes.php#thursday">TeachScheme!</a> in Dallas BC</li>
-<li>Paper sessions start at 10:45am today</li>
-<li>First timers lunch, 12pm in Lone Star A1</li>
-<li>Find your way around with <a href="/sigcse2011/attendees/floor_plans.php">floor plans</a></li>
+<p><strong>Friday March 11th at SIGCSE 2011</strong><br/><ul>
+<li><a href="http://db.grinnell.edu/sigcse/sigcse2011/Program/programAtAGlance.asp?dateRequested=Friday">Friday's full program</a></li>
+<li>Keynote: Susan Landau, 8:30am. <a href="/sigcse2011/attendees/keynotes.php#friday">A Computer Scientist Goes to Washington: How to be Effective in a World Where Facts are 10% of the Equation</a> in Dallas BC</li>
+<li><a href="http://upe.acm.org/">UPE national meeting</a>, 2011 Abacus Award Presentation and Talk: Donald Knuth (12:45-1:35pm)</li>
 </ul></p>
 </div>
