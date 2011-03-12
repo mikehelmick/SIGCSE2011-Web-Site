e193159f1a9b359394f8a1a5e794124953d3119d
Mike Helmick <mike.helmick@gmail.com>
2011-03-12 08:39:43 -0600

    add evaluations to saturday highlights

--- parts/today.php
+++ parts/today.php
@@ -1,5 +1,6 @@
 <div class="GoodMessage">
 <p><strong>Saturday March 12th at SIGCSE 2011</strong><br/><ul>
+<li>Please take a few minutes to <a href="/sigcse2011/attendees/evaluations.php">fill out the SIGCSE 2011 symposium evaluation</a>. Thank you for your participation.</li>
 <li><a href="http://db.grinnell.edu/sigcse/sigcse2011/Program/programAtAGlance.asp?dateRequested=Saturday">Saturday's full program</a></li>
 <li>Keynote: Luis Von Ahn, 12:30pm (<em>luncheon</em>). <a href="/sigcse2011/attendees/keynotes.php#saturday">Three Human Computation Projects</a> in Dallas BCD</li>
 <li><strong>Room Changes</strong><ul>
