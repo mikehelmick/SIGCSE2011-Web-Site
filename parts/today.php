ec57aa41c63b9d1dd43b683a16556f111f552a78
Mike Helmick <mike.helmick@gmail.com>
2011-03-12 11:10:05 -0600

    add robot hoedown links to saturday highlights

--- parts/today.php
+++ parts/today.php
@@ -3,6 +3,10 @@
 <li>Please take a few minutes to <a href="/sigcse2011/attendees/evaluations.php">fill out the SIGCSE 2011 symposium evaluation</a>. Thank you for your participation.</li>
 <li><a href="http://db.grinnell.edu/sigcse/sigcse2011/Program/programAtAGlance.asp?dateRequested=Saturday">Saturday's full program</a></li>
 <li>Keynote: Luis Von Ahn, 12:30pm (<em>luncheon</em>). <a href="/sigcse2011/attendees/keynotes.php#saturday">Three Human Computation Projects</a> in Dallas BCD</li>
+<li><strong>Robot Hoedown and Rodeo</strong><ul>
+ <li>Robot Hoedown Finale, Outside of Dallas A, 12:10 - 12:30pm</li>
+ <li><a href="http://elvis.rowan.edu/~kay/sigcse/media.html">Photos and Videos</a></li>
+</ul></li>
 <li><strong>Room Changes</strong><ul>
  <li>10:55-12:10  Intro CS: Panoptic Views is moving from Dallas D2	to <strong>Lone Star A1</strong></li>
  <li>10:55-12:10  Software Design and Development is moving from Dallas D1 to <strong>Dallas A3</strong></li>
